0e138a5787793ca6811e5ac3f9dc3618b0a50b05
Yannick Huerre <dev@sheoak.fr>
2015-12-23 10:46:26 +0100

    fix: Update example configuration for 0.4

--- examples/config.example.php
+++ examples/config.example.php
@@ -4,6 +4,7 @@
 // @link http://docs.guzzlephp.org/en/latest/request-options.html
 $config = array(
     'base_uri'    => 'https://api.crunchmail.net/v1/',
+    'token_uri'   => 'https://api.crunchmail.net/api-token-auth',
     'client_uri'  => '/customers/customer-id/',
     'connect_timeout' => 10,
      // 'debug' => true, // enable debug
